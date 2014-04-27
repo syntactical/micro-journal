d0cc04adb869b97ae09125cd6dd279a0f407f346
syntactical <greg.dutcher@gmail.com>
2014-04-26 21:22:39 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MicroJournal::Application.config.secret_key_base = 'beb785a0fff053c0170f90770c854efb0f3b20345e9f0a5e729c44eca5c71f642944a394608234db9ea00995e9d927abacec2d6f1c5684dd7dc3e42b844297f5'
